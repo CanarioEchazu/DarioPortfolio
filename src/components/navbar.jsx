20d8811dc204af3d1aca2d6488fc0734ea3be14b
CanarioEchazu <darioechazu@hotmail.com>
2023-02-11 01:58:37 -0300

    eighth commit all ok

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,53 +1,60 @@
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from '../assets/logo.png'
+import React from "react";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Logo from "../assets/logo.png";
 
-const Navbar = () => {
-    const navbarMenu = [
-        {
-            name: 'Home',
-            link: "#"
-        },        {
-            name: 'About',
-            link: "#about"
-        },        {
-            name: 'Services',
-            link: "#services"
-        },        {
-            name: 'Testomonials',
-            link: "#testimonials"
-        },
-    ]
-    return(
-        <div className='flex items-center justify-between px-10 pt-[45px]'>
-            <div className='flex gap-x-[100px]'>
-                <a href='/'>
-                    <img
-                        src={logo}
-                        alt='logo'
-                        className= "h-[40px] w-[40px] rounded-md lg:rounded-lg xl:h-[60px] xl:w-[60px" 
-                        />
-                </a>
-                <div className='hidden items-center gap-x-[50px] font-roboto font-semibold text-white/80 lg:flex lg:text-base xl:text-xl'>
-                    {navbarMenu.map((menu, i) => (
-                        <a href={menu.link} key={i} className='hover:text-white'>
-                            {menu.name}
-                        </a>
-                    ))}
+const Navbar = ({ handleSidebar }) => {
+  const navbarMenu = [
+    {
+      name: "Home",
+      link: "#",
+    },
+    {
+      name: "About",
+      link: "#about",
+    },
+    {
+      name: "Services",
+      link: "#services",
+    },
+    {
+      name: "Testimonials",
+      link: "#testimonials",
+    },
+  ];
 
-                </div>
-            </div>
-            <div className="hidden gap-x-{30px] text-white lg:flex">
-                <div className='flex h-[40px] w-[40px] cursor-pointer items-center justify-center rounded-full bg-dark opacity-80 hover:opacity-100'>
-                        <FontAwesomeIcon icon={faPhone} className="-rotate-90" />
-                </div>
-                <a href='/#contact-me' className='rounded-lg bg-accent/90 px-4 py-2 font-poppins font-bold hover:bg-accent lg:text-base xl:text-lg'>
-                        Contact Me
-                </a>
-
-            </div>
+  return (
+    <div className="flex items-center justify-between px-10 pt-45px">
+      <div className="flex gap-x-100px">
+        <a href={"/"}>
+          <img
+            src={Logo}
+            className="h-40px w-40px rounded-md lg:rounded-lg xl:h-60px xl:w-60px"
+            alt="Logo"
+          />
+        </a>
+        <div className="hidden items-center gap-x-50px font-roboto font-semibold text-white/80 lg:flex lg:text-base xl:text-xl">
+          {navbarMenu.map((menu, i) => (
+            <a href={menu.link} key={i} className="hover:text-white p-4">
+              {menu.name}
+            </a>
+          ))}
         </div>
-    );
+      </div>
+      <div className="hidden gap-x-30px text-white lg:flex">
+        <div className="flex h-40px w-40px cursor-pointer items-center justify-center rounded-full bg-dark opacity-80 hover:opacity-100">
+          <FontAwesomeIcon icon={faPhone} className="-rotate-90" />
+        </div>
+        <a
+          href={"/#contact-me"}
+          className="rounded-lg bg-accent/90 px-4 py-2 font-poppins font-bold hover:bg-accent lg:text-base xl:text-lg"
+        >
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
 };
+
 
 export default Navbar;
---
+++ src/components/opacity.jsx
@@ -0,0 +1,11 @@
+const Opacity = ({ show, handleSidebar }) => {
+    return (
+      <div
+        className={`fixed inset-0 z-20 bg-black/80 ${show ? "animate-show" : ""}`}
+        onClick={handleSidebar}
+      />
+    );
+  };
+  
+  export default Opacity;
+  
