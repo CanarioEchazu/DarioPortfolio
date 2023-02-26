09a87b20ef6f9dcb6b969c75419fceff2c624e62
CanarioEchazu <darioechazu@hotmail.com>
2023-02-26 14:01:02 -0300

    portfolio funcionando ok !!!, es el 21 commit jaja

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,39 @@
+import { useState, useEffect } from "react";
+
 const About = () => {
+  const [nameFontSize, setNameFontSize] = useState("5xl");
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 640) {
+        setNameFontSize("2xl");
+      } else if (windowWidth <= 768) {
+        setNameFontSize("3xl");
+      } else if (windowWidth <= 1024) {
+        setNameFontSize("4xl");
+      } else {
+        setNameFontSize("5xl");
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div
-      className="flex items-center justify-center py-20 md:py-[91px]"
-      id="about"
-    >
+    <div className="flex items-center justify-center mt-28 py-20 md:py-[91px]" id="about">
       <div className="px-14 md:max-w-md lg:max-w-[515px]">
+        {/* <div className={`mb-5 font-roboto-slab font-extrabold text-white md:text-4xl lg:text-${nameFontSize}`}>
+          <span>Dario Echazu</span>
+        </div> */}
         <div className="mb-5 font-roboto-slab text-2xl font-extrabold text-accent-lighter md:text-4xl lg:text-5xl">
           <span>Why Hire Me for Your Next Project?</span>
         </div>
         <div className="mb-5 text-justify font-poppins text-[13px] font-medium text-light lg:text-[15px]">
-          <span>
+        <span>
           With two university degrees in Business Administration and Systems Analysis,
           Dario Echazu has a strong track record of being a self-starter and excelling in leadership roles,
           having managed over 20 employees in his previous role as head of the purchasing department,
@@ -23,9 +47,6 @@ const About = () => {
           to push boundaries and exceed expectations
           </span>
         </div>
-        <button className="rounded-md bg-accent/80 py-2 px-4 font-roboto text-xs font-semibold text-white transition-all hover:bg-accent md:rounded-lg md:px-5 md:text-[13px] lg:text-[15px]">
-          Download CV
-        </button>
       </div>
     </div>
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,54 +5,60 @@ import Github from "../assets/github.png";
 // import Facebook from "../assets/facebook.png";
 
 const Hero = () => {
-  const socialMedia = [
-    {
-      initial: <img src={LinkedIn} alt="LinkedIn" className="h-10 w-10" />,
-      link: "https://www.linkedin.com/in/dario-echazu-508466232/",
-    },
-    {
-      initial: <img src={Instagram} alt="Instagram" className="h-10 w-10" />,
-      link: "https://www.instagram.com/dario.echazu/",
-    },
-    {
-      initial: <img src={Github} alt="Github" className="h-10 w-10" />,
-      link: "https://github.com/CanarioEchazu",
-    },
-    // {
-    //   initial: <img src={Facebook} alt="Facebook" className="h-10 w-10" />,
-    //   link: "https://web.facebook.com/dario.echazu.7",
-    // },
-  ];
+const socialMedia = [
+{
+initial: <img src={LinkedIn} alt="LinkedIn" className="h-10 w-10" />,
+link: "https://www.linkedin.com/in/dario-echazu-508466232/",
+},
+{
+initial: <img src={Instagram} alt="Instagram" className="h-10 w-10" />,
+link: "https://www.instagram.com/dario.echazu/",
+},
+{
+initial: <img src={Github} alt="Github" className="h-10 w-10" />,
+link: "https://github.com/CanarioEchazu",
+},
+// {
+// initial: <img src={Facebook} alt="Facebook" className="h-10 w-10" />,
+// link: "https://web.facebook.com/dario.echazu.7",
+// },
+];
 
-  return (
-    <div className="relative">
-      <div className="inset-x-0 mt-8 flex items-center justify-center font-roboto-slab text-[30px] font-black uppercase tracking-tighter text-white md:absolute md:top-[15%] md:mt-[unset] md:text-[145px] lg:top-[10%] lg:text-[180px] xl:top-[15%] xl:text-[250px]"
-        style={{minWidth: "400px"}}>
-        <span>Dario Echazu</span>
-      </div>
-      <div className="relative mx-auto flex h-full flex-col-reverse justify-center md:mt-8 md:max-w-none md:flex-row md:gap-x-6 xl:-mt-2">
-        <div className="flex flex-none">
-          <img
-            src={Person}
-            alt="Person"
-            className="-mt-14 w-[250px] object-contain sm:w-[345px] md:mt-auto md:w-[400px] lg:w-[450px] xl:w-[560px]"
-          />
-        </div>
-        <div className="relative mx-10 self-end font-poppins sm:mx-14 md:mx-0 md:pr-8 lg:pr-0 lg:pb-16">
-          <div className="relative mb-4 ml-auto w-max text-2xl font-extrabold uppercase text-accent md:text-[32px] lg:mb-6 lg:text-[50px]">
-            <div className="absolute top-full right-0 h-[2px] w-3/4 bg-accent md:top-[120%] lg:top-[150%]" />
-            <span>Full Stack</span>
-          </div>
-          <div className="mb-4 text-justify text-[13px] font-medium text-light lg:max-w-[340px] lg:text-[15px] ">
-            <span>
-              I am passionate about continuously improving and growing while
-              utilizing my productive knowledge to make a positive impact. I am
-              open to exploring various roles within the industry, but
-              ultimately I am drawn towards positions that challenge me to bring
-              my skills to the next level and contribute to the success of the
+return (
+<div className="relative">
+<div
+className="inset-x-0 mt-8 flex items-center justify-center font-roboto-slab text-[30px] font-black uppercase tracking-tighter text-white md:absolute md:top-[15%] md:mt-[unset] md:text-[145px] lg:top-[10%] lg:text-[180px] xl:top-[15%] xl:text-[250px]"
+style={{ minWidth: "400px" }}
+>
+<span>Dario Echazu</span>
+</div>
+<div className="relative mx-auto flex h-full flex-col-reverse justify-center md:mt-8 md:max-w-none md:flex-row md:gap-x-6 xl:-mt-2">
+<div className="flex flex-none">
+<img
+         src={Person}
+         alt="Person"
+         className="-mt-14 w-[250px] object-contain sm:w-[345px] md:mt-auto md:w-[400px] lg:w-[450px] xl:w-[560px]"
+       />
+</div>
+<div className="relative mx-10 self-end font-poppins sm:mx-14 md:mx-0 md:pr-8 lg:pr-0 lg:pb-16">
+<div className="relative mb-4 ml-auto w-max text-2xl font-extrabold uppercase text-accent md:text-[32px] lg:mb-6 lg:text-[50px]">
+<div className="absolute top-full right-0 h-[2px] w-3/4 bg-accent md:top-[120%] lg:top-[150%]" />
+<span>Full Stack</span>
+</div>
+<div className="mb-8 text-justify text-[13px] font-medium text-light lg:max-w-[340px] lg:text-[15px] md:pl-10">
+<span>
+I am passionate about continuously improving and growing while
+utilizing my productive knowledge to make a positive impact. I am
+open to exploring various roles within the industry, but
+ultimately I am drawn towards positions that challenge me to bring
+my skills to the next level and contribute to the success of the
 team.
-            </span>
-          </div>
+</span>
+</div>
+
+
+
+
           <div className="text-right text-[13px] font-semibold text-accent md:mb-20 md:text-left lg:text-base">
             <div>
               <span>+54 9 387 453 33 37</span>
