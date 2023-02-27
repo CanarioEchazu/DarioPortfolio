56af03da485a455213f92394b08973ea432cce06
CanarioEchazu <darioechazu@hotmail.com>
2023-02-27 16:56:44 -0300

    Yes 27-2 Third finish commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -29,27 +29,16 @@ const About = () => {
       id="about"
     >
       <div className="px-4 md:max-w-md md:px-14 lg:max-w-[515px]">
-        {/* <div className={`mb-5 font-roboto-slab font-extrabold text-white md:text-4xl lg:text-${nameFontSize}`}>
-          <span>Dario Echazu</span>
-        </div> */}
         <div className="mb-5 font-roboto-slab text-2xl font-extrabold text-accent-lighter md:text-4xl lg:text-5xl">
           <span>Why Hire Me for Your Next Project?</span>
         </div>
         <div className="mb-5 text-justify font-poppins text-[13px] font-medium text-light lg:text-[15px]">
           <span>
-            With two university degrees in Business Administration and Systems
-            Analysis, Dario Echazu has a strong track record of being a
-            self-starter and excelling in leadership roles, having managed over
-            20 employees in his previous role as head of the purchasing
-            department, and over 30 employees as a supervisor in a casino, where
-            he was also responsible for handling large sums of money. Despite
-            overcoming cancer after 20 rounds of chemotherapy in 2011, Dario has
-            continued to grow and develop professionally by updating his
-            full-stack development skills through platforms like Platzy and also
-            at SoyHenry.com . He brings a combination of determination, skill,
-            and a desire to inspire growth and excellence within the team he
-            works with. In short, he is a committed professional who is always
-            looking for ways to push boundaries and exceed expectations
+          I have two university degrees: one in Business Administration and another as a Systems Analyst. I have been familiar with the ZACHMAN - IDEF1X methodology since 1991, and have applied it in work for major companies such as Alto Noa Shopping in Salta, managing data to achieve better results for the company.
+
+I have a solid track record as an entrepreneur and in leadership roles, having managed over 20 employees in my previous role as Head of the Purchasing Department and over 30 employees as a supervisor in a casino, where I was also responsible for handling large sums of money.
+
+I have continued to grow and develop professionally by updating my FULL-STACK developer skills through platforms like Platzy.com and SoyHenry.com. I have a combination of determination, skill, and a desire to inspire growth and excellence within the team I work with. In summary, I consider myself a committed professional who always seeks ways to push boundaries and meet the expectations the company requires.
           </span>
         </div>
       </div>
